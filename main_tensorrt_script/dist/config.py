2e97a80007b2fe088af2589edfe95d412423edf8
Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>
2024-10-31 13:09:36 -0400

    Add files via upload
    Signed-off-by: Bubbles The Dev <152947339+KernFerm@users.noreply.github.com>

---
+++ main_tensorrt_script/dist/config.py
@@ -0,0 +1,40 @@
+# Screen Capture Settings
+screenShotHeight = 320
+screenShotWidth = 320
+
+# Mask Settings
+useMask = False
+maskSide = "left"
+maskWidth = 80
+maskHeight = 200
+
+# Mouse Movement Amplifier
+aaMovementAmp = 0.4
+
+# Detection Settings
+confidence = 0.4
+
+# Key Bindings
+aaQuitKey = "8"
+aaActivateKey = "CapsLock"
+
+# Targeting Mode
+headshot_mode = True
+
+# Debugging and Visuals
+cpsDisplay = False
+visuals = True
+
+# Target Selection
+centerOfScreen = True
+
+# ONNX Model Selection
+onnxChoice = 3
+model_path = 'v5.engine'
+device = 'cuda'
+fp16 = True
+
+# Arduino Support
+arduino_support = True
+arduino_port = 'COM3'
+arduino_baudrate = 9600
